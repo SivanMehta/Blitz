b7b3134a09741c81e3ff66d6f0d0019ab0850d64
Sivan Mehta <skmehta@andrew.cmu.edu>
2015-09-06 19:32:25 -0400

    adds intial hand data analysis

--- game.py
+++ game.py
@@ -1,5 +1,23 @@
-from blitz import * 
+from blitz import *
+from matplotlib import pyplot as plt
+import sys
 
-game = Blitz()
+def avg(scores):
+    return sum(scores) * 1.0 / len(scores)
 
-print(game.get_players_score())
+print "Generating data on initial hands..."
+
+scores = []
+
+for i in range(100000):
+    game = Blitz()
+    scores.append(avg(game.get_players_score()))
+
+    sys.stdout.flush()
+    sys.stdout.write("\r\t%2.2f%% of trials run... " % ((i+1)*1.0/1000))
+
+print "done!"
+
+print "Average Score: ", avg(scores)
+plt.hist(scores, bins = 100, normed = True)
+plt.show()
